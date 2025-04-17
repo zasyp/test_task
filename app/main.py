225a45fd03a8b1f4694cfa068e3f78255f849131
Yuri <zaspkin4514@gmail.com>
2025-04-17 22:46:41 +0300

    done

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from api import tables, reservations
 
@@ -5,3 +6,6 @@ app = FastAPI()
 
 app.include_router(tables.router, prefix="/api/v1")
 app.include_router(reservations.router, prefix="/api/v1")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)
